ac31b90a3600368f9dbd134e3c0b9861a7828295
qinxhannah <qin.xia@northeastern.edu>
2023-02-23 18:38:58 -0800

    used slf4j logger to log receive messages, added logback.xml to define log pattern and colors

--- assignment2/assignment2-consumer/src/main/java/ConsumerThread.java
+++ assignment2/assignment2-consumer/src/main/java/ConsumerThread.java
@@ -1,20 +1,18 @@
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.DeliverCallback;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ConsumerThread implements Runnable {
   private final String queueName;
   private final Connection con;
   private final Gson gson = new Gson();
   private final ConsumerUtils consumerUtils;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerThread.class);
 
   public ConsumerThread(String queueName, Connection con, ConsumerUtils consumerUtils){
     this.queueName = queueName;
@@ -29,13 +27,12 @@ public class ConsumerThread implements Runnable {
       Channel channel = con.createChannel();
       channel.queueDeclare(queueName, false, false, false, null);
       channel.basicQos(10); // max 10 message per receiver
-
       final DeliverCallback deliverCallback = (consumerTag, delivery)-> {
         String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
         SwipeMessage swipeMessage = gson.fromJson(message, SwipeMessage.class);
         consumerUtils.processSwipeMessage(swipeMessage);
         channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-        System.out.println(Thread.currentThread().getId() + " - thread received " + message);
+        LOGGER.info(Thread.currentThread().getId() + " - thread received " + message);
       };
       channel.basicConsume(this.queueName, false ,deliverCallback, (consumerTag) -> {});
     } catch (IOException e) {
