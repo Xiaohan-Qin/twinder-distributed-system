0fc2fa89728d57c65d19f3b8e57943485d6ad637
qinxhannah <qin.xia@northeastern.edu>
2023-02-23 18:22:16 -0800

    edited Constant to include number of channels, edited rabbitmq config to include virtual host name

--- assignment2/assignment2-server/src/main/java/Constant.java
+++ assignment2/assignment2-server/src/main/java/Constant.java
@@ -4,6 +4,7 @@ public class Constant {
   public static final String MISSING_PARAMS = "Missing parameters";
   public static final String INVALID_PARAMS = "Invalid parameters";
   public static final String INVALID_REQ_BODY = "Invalid request body";
+  public static final int NUM_CHANNEL = 10;
   public static final int SWIPER_LOWER_BOUND = 1;
   public static final int SWIPER_UPPER_BOUND = 5000;
   public static final int SWIPEE_LOWER_BOUND = 1;
