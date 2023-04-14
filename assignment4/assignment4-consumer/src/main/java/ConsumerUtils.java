4545a09b8479f9c1394f589f0047ca890180610c
qinxhannah <qin.xia@northeastern.edu>
2023-04-13 23:01:04 -0700

    edited the write logic. Use one SELECT statement instead of two to minimize latency.

--- assignment4/assignment4-consumer/src/main/java/ConsumerUtils.java
+++ assignment4/assignment4-consumer/src/main/java/ConsumerUtils.java
@@ -1,6 +1,7 @@
 import java.sql.*;
 import java.sql.PreparedStatement;
 import ConnectionManagers.DatabaseConnectionPool;
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,29 +33,20 @@ public class ConsumerUtils {
         stmt.setInt(1, message.getSwiperId());
         stmt.setInt(2, message.getLeftOrRight().equals("right") ? 1 : 0);
         stmt.setInt(3, message.getLeftOrRight().equals("left") ? 1 : 0);
-        stmt.setArray(4, dbConnection.createArrayOf("integer", new Integer[0]));
+        stmt.setArray(4, dbConnection.createArrayOf("integer", message.getLeftOrRight().equals("right") ? new Integer[]{message.getSwipeeId()} : new Integer[0]));
       } else {
-        stmt = dbConnection.prepareStatement(
-            "SELECT * FROM users.user_data WHERE userid = ? AND ? = ANY(matched_users)");
-        stmt.setInt(1, message.getSwiperId());
-        stmt.setInt(2, message.getSwipeeId());
-        rs = stmt.executeQuery();
-        boolean alreadySwiped = rs.next();
-        if (message.getLeftOrRight().equals("right")) {
-          if (alreadySwiped) {
-            // If the swiper has already swiped on the swipee, don't update the database
-            dbConnection.commit();
-            return;
+        boolean alreadySwiped = rs.getArray("matched_users").getArray() != null && Arrays.asList((Integer[]) rs.getArray("matched_users").getArray()).contains(message.getSwipeeId());
+        if (!alreadySwiped) {
+          if (message.getLeftOrRight().equals("right")) {
+            stmt = dbConnection.prepareStatement(
+                "UPDATE users.user_data SET num_likes = num_likes + 1, matched_users = array_append(matched_users, ?) WHERE userid = ?");
+            stmt.setInt(1, message.getSwipeeId());
+            stmt.setInt(2, message.getSwiperId());
+          } else {
+            stmt = dbConnection.prepareStatement(
+                "UPDATE users.user_data SET num_dislikes = num_dislikes + 1 WHERE userid = ?");
+            stmt.setInt(1, message.getSwiperId());
           }
-          stmt = dbConnection.prepareStatement(
-              "UPDATE users.user_data SET num_likes = num_likes + 1, matched_users = array_append(matched_users, ?) WHERE userid = ?");
-          stmt.setInt(1, message.getSwipeeId());
-          stmt.setInt(2, message.getSwiperId());
-        } else {
-          // If the swiper swiped left, increment num_dislikes
-          stmt = dbConnection.prepareStatement(
-              "UPDATE users.user_data SET num_dislikes = num_dislikes + 1 WHERE userid = ?");
-          stmt.setInt(1, message.getSwiperId());
         }
       }
       stmt.executeUpdate();
